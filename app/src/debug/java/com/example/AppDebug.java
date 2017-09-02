1b55fcb925c6baf6c26054ba7b3c7c7b530e0310
Vitor Hugo Salgado <vitor.salgado@concrete.com.br>
2017-09-02 01:54:17 -0300

    travis; flowup.

--- app/src/debug/java/com/example/AppDebug.java
+++ app/src/debug/java/com/example/AppDebug.java
@@ -6,25 +6,32 @@ import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
+import io.flowup.FlowUp;
+
 public class AppDebug extends App {
-    @Override
-    public void onCreate() {
-        super.onCreate();
+	@Override
+	public void onCreate() {
+		super.onCreate();
 
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return;
-        }
+		if (LeakCanary.isInAnalyzerProcess(this)) {
+			return;
+		}
 
-        FacebookSdk.setIsDebugEnabled(true);
-        FacebookSdk.addLoggingBehavior(LoggingBehavior.DEVELOPER_ERRORS);
+		FacebookSdk.setIsDebugEnabled(true);
+		FacebookSdk.addLoggingBehavior(LoggingBehavior.DEVELOPER_ERRORS);
 
-        Stetho.initializeWithDefaults(this);
-    }
+		Stetho.initializeWithDefaults(this);
 
-    @Override
-    protected RefWatcher setUpLeakCanary() {
-        return LeakCanary.refWatcher(this)
-                .buildAndInstall();
-    }
+		FlowUp.Builder.with(this)
+				.apiKey(BuildConfig.FLOWUP_KEY)
+				.forceReports(BuildConfig.DEBUG)
+				.start();
+	}
+
+	@Override
+	protected RefWatcher setUpLeakCanary() {
+		return LeakCanary.refWatcher(this)
+				.buildAndInstall();
+	}
 
 }
